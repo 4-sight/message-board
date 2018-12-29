341b808148d6da28a6864afa92ca77eb6410aadd
Peter Newton <peter@4sightdesigns.com>
2018-12-29 15:59:05 +0000

    Setup route logic

--- routes/api.js
+++ routes/api.js
@@ -8,43 +8,45 @@
 
 'use strict';
 
+const createBoard = require('../models/thread')
 
 module.exports = function (app) {
   
   app.route('/api/threads/:board')
 
     .post(async (req, res) => {
-
+      const Board = createBoard(req.params.board)
     })
 
     .get(async (req, res) => {
-
+      const Board = createBoard(req.params.board)
+      res.send('Hello')
     })
 
     .delete(async (req, res) => {
-
+      const Board = createBoard(req.params.board)
     })
 
     .put(async (req, res) => {
-
+      const Board = createBoard(req.params.board)
     })
     
   app.route('/api/replies/:board')
 
     .post(async (req, res) => {
-
+      const Board = createBoard(req.params.board)
     })
 
     .get(async (req, res) => {
-
+      const Board = createBoard(req.params.board)
     })
 
     .delete(async (req, res) => {
-
+      const Board = createBoard(req.params.board)
     })
 
     .put(async (req, res) => {
-      
+      const Board = createBoard(req.params.board)
     })
 
 };
