45eb389e892584275a4c67b9696c28adf978ac78
Peter Newton <peter@4sightdesigns.com>
2018-12-30 14:05:24 +0000

    Add post reply logic

--- models/thread.js
+++ models/thread.js
@@ -1,35 +1,66 @@
 const mongoose = require('mongoose')
 
-const threadSchema = new mongoose.Schema({
-    text: String,
-    created_on: {type: Date, default: Date.now },
-    bumped_on: {type: Date, default: Date.now },
-    reported: {type: Boolean, default: false},
-    delete_password: String,
-    replies: [String]
+const replySchema = new mongoose.Schema({
+  text: String,
+  created_on: { type: Date, default: Date.now },
+  delete_password: String,
+  reported: { type: String, default: false }
 })
 
-threadSchema.pre('save', next => {
-    let now = new Date();
-    this.bumped_on = now;
-    if (!this.created_on) {
-        this.created_on = now
-    }
-    next()
+const threadSchema = new mongoose.Schema({
+  text: String,
+  created_on: {type: Date, default: Date.now },
+  bumped_on: {type: Date, default: Date.now },
+  reported: {type: Boolean, default: false},
+  delete_password: String,
+  replies: [replySchema]
+})
+
+let now = new Date()
+
+replySchema.pre('save', function(next) {
+  if (!this.created_on) {
+    this.created_on = now
+  }
+  next()
+})
+
+threadSchema.pre('save', function(next) {
+  this.bumped_on = now;
+  if (!this.created_on) {
+    this.created_on = now
+  }
+  next()
 })
 
 const createBoard = (boardName) => {
-    const Board = mongoose.model('board', threadSchema, boardName)
+  const Board = mongoose.model('board', threadSchema, boardName)
 
     //Methods
-    Board.createThread = function(text, password) {
-        return this.create({
-            text: text,
-            delete_password: password,
-        })
-    }
+  Board.createThread = function(text, password) {
+    return this.create({
+      text: text,
+      delete_password: password,
+    })
+  }
 
-    return Board
+  Board.createReply = function(threadId, text, password) {
+    return this.findByIdAndUpdate(
+      threadId,
+      { $push: {
+         replies: { 
+          text: text,
+          delete_password: password
+          }
+        },
+        $set: {
+          bumped_on: now
+        }
+      }
+    )
+  }
+
+  return Board
 }
 
 module.exports = createBoard
--- routes/api.js
+++ routes/api.js
@@ -29,7 +29,18 @@ module.exports = function (app) {
 
     .get(async (req, res) => {
       const Board = createBoard(req.params.board)
-      res.send('Hello')
+      res.json({
+        _id: "2",
+        text: "test text",
+        created_on: "Tuesday",
+        bumped_on: "later on tuesday",
+        replies: [
+          {
+            text: "hello",
+            created_on: "wednesday"
+          }
+        ]
+      })
     })
 
     .delete(async (req, res) => {
@@ -39,11 +50,22 @@ module.exports = function (app) {
     .put(async (req, res) => {
       const Board = createBoard(req.params.board)
     })
-    
+  
+  //=================================================================
+
   app.route('/api/replies/:board')
 
     .post(async (req, res) => {
-      const Board = createBoard(req.params.board)
+      const { board } = req.params
+      const { text, delete_password, thread_id } = req.body
+      const Board = createBoard(board)
+
+      try{ await Board.createReply(thread_id, text, delete_password) }
+      catch(err){
+        console.error(`Failed to post reply to ${board}`) 
+        res.send('Error occurred while saving reply')
+      }
+      res.redirect(`/b/${board}`)
     })
 
     .get(async (req, res) => {
